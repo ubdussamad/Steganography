dde2ee85fd2cf76cd1a06f278edd37ed9a9ddd41
ubdussamad <ubdussamad@gmail.com>
2018-10-12 13:20:27 +0530

    Everything works

--- dec.py
+++ dec.py
@@ -1,36 +1,40 @@
+#!/usr/bin/env python3
+
+'''-------------------------------------------------------------
+* Pixel Wise Image Analyzer cum Decrypter
+* (Recovers data embedded in an image.)
+* 07OCT2018
+* Authors: Mohammed S. Haque <ubdussamad@gmail.com>
+* Primarily Built around Python3.x
+
+* Usage:
+* user@machine~$ ./dec.py
+* It'll automatically save the decrypted data as an extension-less file named: recovered_data
+*
+* This program currently utilizes an even odd scheme to store/read binary data into/from an image.
+*
+* Working Principle:
+* Reads a Portable Network Graphic (.png) by converting it to a 2D array.
+* An odd color value represents binary 0 and an even color value represents binary 1.
+* Primarily color values in the image's matrix are converted into odd integer by adding
+* integer value 1 if the color value is even else 0 if the color value is already odd.
+------------------------------------------------------------------------------------------'''
+
 from PIL import Image
 import numpy as np
-import time
-
+import time,math
 img = Image.open('secret.png') #Reading the Image
 arr = np.array(img) #Converting the Image into a 2D array
 x,y,z  = np.shape(arr) #Reading the dimensions of the array to determine the storage.
 arr = arr.ravel() & 1 #Converting every value in the matrix to 0 if it's even else 1.
-lis = arr ^ 1 #Noting every value in the matix.
-
-index = 19955
-import math
-print("The diamentions of the image are: %dx%dx%d pixels."%(x,y,z))
-jj = lis[:index+1]
-ll = len(jj)
-lis = np.array_split( jj , math.ceil(ll/8) ) #Splitting the array into groups of 8 bit binary values.
-print(1)
-
-
-lis = lis#Terminating the list at the pointer very all 8 bits are 0
-print(2)
-print(lis[0])
-
-tmp = []
-for i in lis:
-    out = 0
-    for bit in i:
-        out = (out << 1) | bit
-    tmp.append(out)
-print(3)
-lis = tmp
-bytes_array = bytearray( lis )
-print(max(lis))
-f = open('new_p.jpg','wb')
-f.write(bytes_array)
-f.close()
+data_array = arr ^ 1 #Noting every value in the matrix.
+reserved_bit_width =  math.ceil(math.log2((x*y*3)/8))
+print("The dimensions of the image are: %dx%dx%d pixels."%(x,y,z))
+data_bytes = int(''.join(map(str , data_array[:reserved_bit_width])),2)
+print("The enclosed data is of size: %d Bytes"%(data_bytes))
+data_array = data_array[ reserved_bit_width : (data_bytes * 8) + reserved_bit_width]
+data_array = bytearray(np.packbits(data_array))
+print("Start byte:%d / Mid byte: %d / End Byte:%d"%(data_array[0],data_array[1],data_array[-1]))
+with open('recovered_data','wb') as f_obj:
+	f_obj.write(data_array)
+print('_'*45,"\nEnclosed data saved as: recovered_data")
--- enc.py
+++ enc.py
@@ -1,46 +1,52 @@
 #!/usr/bin/env python3
+
+'''-------------------------------------------------------------
+* Pixel Wise Image Modification cum Encryption Utility.
+* (Embeds String data into an image.)
+* 07OCT2018
+* Authors: Mohamed S. Haque <ubdussamad@gmail.com>
+* Primarily Built around Python3.x
+*
+* Usage:
+* user@machine~$ ./enc.py [path_to_image_which_will_store_data] [path_to_file_which_is_to_be_enclosed]
+* The output Image will be saved as secret.png in the same directory as this file.
+*
+* This program currently utilizes an even odd scheme to store/read binary data into/from an image.
+*
+* Working Principle:
+* Reads a Portable Network Graphic (.png) and converts it to a 2D array.
+* An odd color value represents binary 0 and an even color value represents binary 1.
+* Primarily color values in the image's matrix are converted into odd integer by adding
+* integer value 1 if the color value is even else 0 if the color value is already odd.
+*
+* Scheme: Odd if 0 , even if 1
+------------------------------------------------------------------------------------------'''
+
+
 from PIL import Image
 import numpy as np
-import time,math
-
-
-img = Image.open('Desktop/test.jpg') #Reading the Image
-arr = np.array(img) #Converting the Image into a 2D array
+import math,sys
+storage_path,path_string = sys.argv[1:3]
+arr = np.array(Image.open(storage_path)) #Converting the Image into a 2D array
 x,y,z  = np.shape(arr) #Reading the dimensions of the array to determine the storage.
 arr |= 1 #Making every value in the matrix odd
-
-print("The amount of data you can store in this image is: %d kiBs"%((x*y*z)/(1024*8)))
-
 reserved_bit_width =  math.ceil(math.log2((x*y*3)/8))
-
-path_string = 'test.jpg'#input("Enter the path:") # Scanning the string to be stored.
-
-long_array = [] #Array containing the binary values.
-
 with open(path_string,'rb') as f_obj:
-	tmp = f_obj.read()
-	f_obj.close()
+	file_data = f_obj.read()
 
-string = ''
+print("The amount of data you can store in this image is: %d KiBs"%((x*y*z)/(1024*8)))
+print("The size of the file is: %d Bytes"%len(file_data))
+print("Start byte:%d, Mid byte: %d End byte: %d"%(file_data[0],file_data[1] , file_data[-1]))
 
-print("performing string stuff.")
-for i in tmp:
-	string += "%s,"%str(i)
-
-print("Done with  string stuff")
-
-long_array = [] #Array containing the binary values.
-
-for i in string:
-    long_array += list(map(int,list(format(ord(i), '#010b')[2:]))) #Storing the characters in binary form (ASCII)
-
-#long_array = np.array([int(i) for i in ''.join([bin(int(format(ord(i), '#010b'),2))[2:] for i in z])])
-
+long_array = np.array([ int(i) for i in format(len(file_data) , '#0%db'%(reserved_bit_width+2) )[2:] ])
+long_array = np.concatenate( ( long_array , np.unpackbits(bytearray(np.array(file_data))) ) , axis = 0 )
 arr.ravel()[:len(long_array)] += np.array(long_array, arr.dtype) #Adds one if there is a one else adds zero hence making even odd pairs. 
 
+print("Asserting Checks.")
+assert( not(  (len(long_array) - reserved_bit_width )%8 ) )
+print("Recreating the Image, Plese wait...\n")
+
 image = Image.fromarray(arr) #Recreating the image from the modified array.
-
-image.show() #Displaying the picture.
-
 image.save("secret.png") #Saving the Image for transfer or future use. Note: The output picture must be in PNG format
 #As other formats try to compress the data corrupting the encoded data within the image.
+print('_'*50,"\nDone Rendering Image.")
